ea01e1f94ba80a5b7b2eccae1970d2208d54d87f
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-01-14 08:39:22 +0000

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, e => {
+    if (e) {
+      res.status(400).send('e');
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
